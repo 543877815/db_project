821ac8b1ca9a860411288af7ba07ae16497b84a2
543877815 <543877815@qq.com>
2018-12-04 20:08:31 +0800

    project init

---
+++ components/public/header/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row
+    :gutter="15"
+    class="m-header">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"/>x
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"/>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from './geo'
+  export default {
+    components: {
+      Geo
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
